d0c4c7515166db16e777bc350f73bde079fc6d17
Vlad Vovk <vlad.vovk@uproad.com>
2023-11-10 19:12:59 +0100

    Queue based on two stacks

--- Sources/QueuePackage/QueuePackage.swift
+++ Sources/QueuePackage/QueuePackage.swift
@@ -1,6 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
+
 public protocol Queue {
     associatedtype Element
     mutating func enqueue(_ element: Element) -> Bool
@@ -9,6 +11,7 @@ public protocol Queue {
     var peek: Element? { get }
 }
 
+//*******************************
 public struct QueueArray<T>: Queue {
     private var array: [T] = []
     public init() {}
@@ -31,7 +34,105 @@ public struct QueueArray<T>: Queue {
 }
 
 extension QueueArray: CustomStringConvertible {
+    public var description: String {
+        String(describing: array)
+    }
+}
+
+//*******************************
+//public class QueueLinkedList<T>: Queue {
+//    private var list = DoublyLinkedList<T>()
+//    public init() {}
+//
+//    public func enqueue(_ element: T) -> Bool {
+//        list.append(element)
+//        return true
+//    }
+//
+//    public func dequeue() -> T? {
+//        guard !list.isEmpty, let element = list.first else {
+//            return nil
+//        }
+//        return list.remove(element)
+//    }
+//
+//    public var peek: T? {
+//        list.first?.value
+//    }
+//
+//    public var isEmpty: Bool {
+//        list.isEmpty
+//    }
+//}
+//
+//extension QueueLinkedList: CustomStringConvertible {
+//    public var description: String {
+//        String(describing: list)
+//    }
+//}
+
+//*******************************
+//public struct QueueRingBuffer<T>: Queue {
+//    private var ringBuffer: RingBuffer<T>
+//
+//    public init(count: Int) {
+//        ringBuffer = RingBuffer<T>(count: count)
+//    }
+//
+//    public var isEmpty: Bool {
+//        ringBuffer.isEmpty
+//    }
+//
+//    public var peek: T? {
+//        ringBuffer.first
+//    }
+//
+//    public mutating func enqueue(_ element: T) -> Bool {
+//        ringBuffer.write(element)
+//    }
+//
+//    public mutating func dequeue() -> T? {
+//        ringBuffer.read()
+//    }
+//}
+//
+//extension QueueRingBuffer: CustomStringConvertible {
+//  public var description: String {
+//   String(describing: ringBuffer)
+//  }
+//}
+//
+//*******************************
+public struct QueueStack<T> : Queue {
+    private var leftStack: [T] = []
+    private var rightStack: [T] = []
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        leftStack.isEmpty && rightStack.isEmpty
+    }
+    
+    public var peek: T? {
+        !leftStack.isEmpty ? leftStack.last : rightStack.first
+    }
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        rightStack.append(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        if leftStack.isEmpty {
+            leftStack = rightStack.reversed()
+            rightStack.removeAll()
+        }
+        return leftStack.popLast()
+    }
+}
+
+extension QueueStack: CustomStringConvertible {
   public var description: String {
-    String(describing: array)
+    String(describing: leftStack.reversed() + rightStack)
   }
 }
