f28110522dfd522a1dc25e46f9d13c2e2e2c6073
Vlad Vovk <vlad.vovk@uproad.com>
2023-11-13 15:52:33 +0100

    no message

--- Sources/QueuePackage/QueuePackage.swift
+++ Sources/QueuePackage/QueuePackage.swift
@@ -5,7 +5,7 @@ import Foundation
 
 public protocol Queue {
     associatedtype Element
-    mutating func enqueue(_ element: Element) -> Bool
+    @discardableResult mutating func enqueue(_ element: Element) -> Bool
     mutating func dequeue() -> Element?
     var isEmpty: Bool { get }
     var peek: Element? { get }
